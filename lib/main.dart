507516df5c39cf25fc2ce66c1d0309d92baa06b3
avishka2k <avishka2k@gmail.com>
2022-07-13 23:21:56 +0530

    Add icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:meeting_app/firebaseAuth/googleAuth.dart';
-import 'package:meeting_app/screens/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
